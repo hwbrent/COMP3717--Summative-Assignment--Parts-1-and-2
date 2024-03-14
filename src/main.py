ce9b849a8bfc6b3db54313bd7067409df8b77105
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-03-14 16:13:37 +0000

    Parse `.hum` file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,31 @@
+import os
+
+from music21 import converter
+
+this_dir = os.path.dirname(__file__)
+Data_dir = os.path.join(
+    this_dir,
+    os.path.pardir,
+    "data",
+    "CoCoPops",
+    "RollingStone",
+    "Data",
+)
+
+
+# ------------------------------------------------------------------------
+
+
 def main():
-    pass
+    file_path = os.path.join(Data_dir, "BobDylan_MrTambourineMan_1965.hum")
+
+    # Need to specify the "humdrum" format, otherwise the parser won't know
+    # how to parse the file
+    score = converter.parse(file_path, format="humdrum")
+    print(score)
+
+
+# ------------------------------------------------------------------------
 
 
 if __name__ == "__main__":
