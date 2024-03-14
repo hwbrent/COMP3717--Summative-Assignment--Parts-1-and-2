f9df02571670fdbfbad15df638c57adf35ca3748
hwbrent <68196383+hwbrent@users.noreply.github.com>
2024-03-14 16:44:44 +0000

    Add utility function to load file

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 
 from music21.converter import parse
+from music21.stream import Score, Part, Opus
 
 this_dir = os.path.dirname(__file__)
 Data_dir = os.path.join(
@@ -12,17 +14,41 @@ Data_dir = os.path.join(
     "Data",
 )
 
+MrTambourineMan = os.path.join(Data_dir, "BobDylan_MrTambourineMan_1965.hum")
+
+
+# ------------------------------------------------------------------------
+
+
+def load_file() -> Score | Part | Opus:
+    """
+    Loads a `.hum` file into a format recognisable by `music21`. If a file
+    path is provided via the command line, that file will be used. Otherwise,
+    we use
+    `data/CoCoPops/RollingStone/Data/BobDylan_MrTambourineMan_1965.hum`
+    """
+
+    hum_path = None
+
+    # If an argument is provided via the command line, and it's a file, use
+    # that as our path
+    args = sys.argv
+    if len(args) == 2 and os.path.isfile(args[1]):
+        hum_path = args[1]
+
+    # Else, default to using Mr Tambourine Man
+    else:
+        hum_path = MrTambourineMan
+
+    return parse(hum_path, format="humdrum")
+
 
 # ------------------------------------------------------------------------
 
 
 def main():
-    file_path = os.path.join(Data_dir, "BobDylan_MrTambourineMan_1965.hum")
-
-    # Need to specify the "humdrum" format, otherwise the parser won't know
-    # how to parse the file
-    score = parse(file_path, format="humdrum")
-    print(score)
+    file = load_file()
+    print(file)
 
 
 # ------------------------------------------------------------------------
